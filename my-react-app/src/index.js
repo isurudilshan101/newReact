26f05e1925f10575b9c0192616345d8225f27ee1
isurudilshan101 <40594182+isurudilshan101@users.noreply.github.com>
2020-08-02 00:21:08 +0530

    comment

--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
+function getButtonText(){
+    return 'click me now!'
+}
+
 const App=()=>{
 
-    const buttonText='click me!';
+    //const buttonText='click me!';
     return <div>
         <label class="lable" for="name">Enter Name</label>
         <input id="name" type="text"/>
         <button style={{backgroundColor:'blue', color:'white'}}>
-            {buttonText}
+            {getButtonText()}
         </button>
         </div>
         
