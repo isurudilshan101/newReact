cdfb2930170d6758fef4e6720931788ca7648ac7
isurudilshan101 <40594182+isurudilshan101@users.noreply.github.com>
2020-08-01 23:50:28 +0530

    comment

--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -2,7 +2,12 @@ import React from 'react';
 import ReactDom from 'react-dom';
 
 const App=()=>{
-    return <div>Hi there,</div>
+    return <div>
+        <label class="lable" for="name">Enter Name</label>
+        <input id="name" type="text"/>
+        <button style="background-color:blue; color:white;">Submit</button>
+        </div>
+        
 };
 
 ReactDom.render(<App/>,document.querySelector('#root'));
