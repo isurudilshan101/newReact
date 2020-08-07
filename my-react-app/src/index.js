39264ba3e5749b982f97e537c44936b736d9f7ed
isurudilshan101 <40594182+isurudilshan101@users.noreply.github.com>
2020-08-07 12:55:45 +0530

    comment

--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -9,7 +9,30 @@ const App=()=>{
 
      
 
-    return <div> Hi there! </div>
+    return (
+        <div className="ui container comments">
+            <div className="comment">
+                <a href="/" className="avatar">
+                    <img alt="avatar"/>
+                </a>
+                <div className="content">
+                    <a href="/" className="author">
+                        sam
+                    </a>
+                    <div className="metadata">
+                        <span className="date">today at 6:00PM</span>
+
+                    </div>
+                    <div className="text">
+                        Nice blog post!
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+    );
         
 };
 
