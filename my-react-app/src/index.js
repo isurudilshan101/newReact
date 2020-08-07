530801d94d4d08fb92bb76efddb19caa5c888717
isurudilshan101 <40594182+isurudilshan101@users.noreply.github.com>
2020-08-07 13:27:02 +0530

    comment

--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import faker from 'faker'; 
 
 // function getButtonText(){
 //     return 'click me now!'
@@ -13,7 +14,7 @@ const App=()=>{
         <div className="ui container comments">
             <div className="comment">
                 <a href="/" className="avatar">
-                    <img alt="avatar"/>
+                    <img alt="avatar" src={faker.image.avatar()}/>
                 </a>
                 <div className="content">
                     <a href="/" className="author">
