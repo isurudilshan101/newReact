8eb2a492dc4d84be40e66ba722b6d962b6a63b69
isurudilshan101 <40594182+isurudilshan101@users.noreply.github.com>
2020-08-08 07:56:54 +0530

    comment

--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -30,6 +30,51 @@ const App=()=>{
 
                 </div>
 
+                
+
+
+            </div>
+
+            <div className="comment">
+                <a href="/" className="avatar">
+                    <img alt="avatar" src={faker.image.avatar()}/>
+                </a>
+                <div className="content">
+                    <a href="/" className="author">
+                        sam
+                    </a>
+                    <div className="metadata">
+                        <span className="date">today at 6:00PM</span>
+
+                    </div>
+                    <div className="text">
+                        Nice blog post!
+                    </div>
+
+                </div>
+            </div>
+            
+            <div className="comment">
+                <a href="/" className="avatar">
+                    <img alt="avatar" src={faker.image.avatar()}/>
+                </a>
+                <div className="content">
+                    <a href="/" className="author">
+                        sam
+                    </a>
+                    <div className="metadata">
+                        <span className="date">today at 6:00PM</span>
+
+                    </div>
+                    <div className="text">
+                        Nice blog post!
+                    </div>
+
+                </div>
+
+                
+
+
             </div>
 
         </div>
